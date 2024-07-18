6a371b4d3d422b1c96d1ceab3db6c435d977246e
omwitsa-etole <omwitsaetole@gmail.com>
2024-07-18 12:23:57 +0300

    edit js

--- main.py
+++ main.py
@@ -74,6 +74,7 @@ def error_merge(tool,key,id):
 
 @app.route("/download/<string:key>/<int:id>")
 def success_download(key,id):
+    
     return render_template("download.html",key=key,id=id,manifest=session["manifest"],apiServer="http://localhost")
 
 @app.route("/")
