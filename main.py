f3aad40eb2a50b89bce6fadbe29f8d5046bff381
omwitsa-etole <omwitsaetole@gmail.com>
2024-07-24 18:30:07 +0300

    edit add ads

--- main.py
+++ main.py
@@ -281,6 +281,14 @@ def function(title):
         return render_template("tools/"+found["tool"]+".html",manifest=session["manifest"],tool=found)
     return render_template("tool.html",manifest=session["manifest"],tool=found)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', error_code=404, error_message='Page Not Found'), 404
+
+# Error handler for 500 errors
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error.html', error_code=500, error_message='Internal Server Error'), 500
 
     
 
